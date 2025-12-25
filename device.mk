1bc210a35de7d21781f36da9a548228a64819f07
Jamie Falconer <mrfluffyoven@gmail.com>
2025-12-25 16:07:42 +0000

    Add dynamic partitions and health packages to device.mk

--- device.mk
+++ device.mk
@@ -6,3 +6,23 @@
 #
 
 LOCAL_PATH := device/samsung/r12s
+
+# Dynamic
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd \
+
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service \
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    update_engine \
+    update_engine_sideload \
+    update_verifier \
